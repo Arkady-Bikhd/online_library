8e8ae73d5d087576bbc53fb37bc1d2399a0522b5
Arkadiy <capark74@mail.ru>
2023-04-15 13:26:00 +0300

    Загрузка книг с tululu.org

--- main.py
+++ main.py
@@ -4,21 +4,22 @@ from pathlib import Path
 
 def main():
 
-    book_url = 'https://tululu.org/txt.php?id=32168'
-    response = requests.get(book_url)
-    response.raise_for_status()
-    filename = 'Пески Марса.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+    book_url = 'https://tululu.org/txt.php?id='
+    first_id = 32168
+    text_dir = 'books'
+    for id in range(10):
+        url_id = f'{book_url}{first_id + id}'
+        text_file = f'id{id}.txt'
+        get_text_file(url_id, text_file, text_dir)    
 
 
-def get_image(url, image_file, image_dir):
+def get_text_file(url, text_file, text_dir):
 
     response = requests.get(url)
     response.raise_for_status()
-    current_dir = Path.cwd() / image_dir
+    current_dir = Path.cwd() / text_dir
     Path(current_dir).mkdir(parents=True, exist_ok=True)
-    file_name = Path() / current_dir / image_file
+    file_name = Path() / current_dir / text_file
     with open(file_name, 'wb') as file:
         file.write(response.content)
 
