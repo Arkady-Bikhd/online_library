69df8d98fe0d996162105f5f1aabb5e972ef5abb
Arkadiy <capark74@mail.ru>
2023-04-16 14:06:31 +0300

    загрузка книг с названиями

--- main.py
+++ main.py
@@ -1,28 +1,66 @@
 import requests
 from pathlib import Path
+from requests.exceptions import HTTPError
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+
 
 
 def main():
 
-    book_url = 'https://tululu.org/txt.php?id='
-    first_id = 32168
-    text_dir = 'books'
+    
+    fetch_books()   
+    
+
+def get_book_title(book_url):
+
+    response = requests.get(book_url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('h1')
+    title_tag_text = title_tag.text.split('::')
+    return title_tag_text[0].strip()
+    # book_title = title_tag_text[0].strip()
+    # book_author = title_tag_text[1].strip()
+    # print(f'Заголовок: {book_title}')
+    # print(f'Автор: {book_author}')
+    #print(soup.f)
+
+
+def fetch_books():
+
+    book_url = 'https://tululu.org/b'
+    text_url = 'https://tululu.org/txt.php?id='
+    first_id = 1
+    folder = 'books'
     for id in range(10):
-        url_id = f'{book_url}{first_id + id}'
-        text_file = f'id{id}.txt'
-        get_text_file(url_id, text_file, text_dir)    
+        try:
+            book_url_id = f'{book_url}{first_id + id}/'
+            filename = f'{first_id+id}. {get_book_title(book_url_id)}'
+            text_url_id = f'{text_url}{first_id + id}'
+            download_txt(text_url_id, filename, folder)
+        except HTTPError: 
+            print('Такой книги нет')     
 
 
-def get_text_file(url, text_file, text_dir):
+def check_for_redirect(response):
+
+    if response.history:
+        raise HTTPError
+
+
+def download_txt(url, filename, folder):
 
     response = requests.get(url)
     response.raise_for_status()
-    current_dir = Path.cwd() / text_dir
+    check_for_redirect(response)
+    filename = f'{sanitize_filename(filename)}.txt'
+    current_dir = Path.cwd() / folder
     Path(current_dir).mkdir(parents=True, exist_ok=True)
-    file_name = Path() / current_dir / text_file
-    with open(file_name, 'wb') as file:
+    filepath = Path() / current_dir / filename
+    with open(filepath, 'wb') as file:
         file.write(response.content)
-
 
 if __name__ == '__main__':
 
