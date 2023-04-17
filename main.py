cd23526c4501389c2317210377c79685c5fee8c3
Arkadiy <capark74@mail.ru>
2023-04-17 16:21:42 +0300

    Добавлен ввод с командной строки

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 from urllib.parse import urljoin
+import argparse
 
 
 
 def main():
-
     
     fetch_books()   
     
@@ -39,16 +39,17 @@ def parse_book_page(soup):
 
 def fetch_books():
 
+    book_ids = get_initial_args()
     books_folder = 'books'
     images_folder = 'images'
-    for id in range(1, 10):
+    for id in range(book_ids[0], book_ids[1]+1):
         try:
             book_feateres = parse_book_page(get_book_html(id))
             download_txt(id, book_feateres['book_title'], books_folder)
             download_image(id, book_feateres['image_src'], images_folder)
             print_book_features(book_feateres)
         except HTTPError: 
-            print('Такой книги нет')     
+            print('Книга с таким номером не найдена')     
 
 
 def check_for_redirect(response):
@@ -94,6 +95,17 @@ def print_book_features(book_features):
     for comment in book_features["book_comments"]:
         print(comment)    
 
+
+def get_initial_args():
+     
+    parser = argparse.ArgumentParser(
+        description='Программа формирует элетронную библиотеку',               
+    )
+    parser.add_argument('start_id', type=int, help='Номер начальной книги')
+    parser.add_argument('end_id', type=int, help='Номер последней книги')
+    args = parser.parse_args()
+    return args.start_id, args.end_id
+    
 
 if __name__ == '__main__':
 
