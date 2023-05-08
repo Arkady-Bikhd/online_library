d9a43ff66e1253104980e137a2fbd64ee60970b1
Arkadiy <capark74@mail.ru>
2023-05-08 10:39:25 +0300

    Разделен список книг по страницам

--- render_website.py
+++ render_website.py
@@ -2,26 +2,26 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
 from more_itertools import chunked
+from pathlib import Path
 
-def load_book_feateres():
-
-    with open("books.json", "r") as json_file:
-        json_book_feateres = json_file.read()
-    json_book_feateres = json.loads(json_book_feateres)
-    book_feateres = list()
-    for books in json_book_feateres:
-        for book in books:
-            book_feateres.append(book)       
-    return list(chunked(book_feateres, 2))            
     
 def on_reload():
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-    template = env.get_template('template.html')
-    rendered_page = template.render(
-        book_feateres = load_book_feateres(),
-    )
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+
+    with open("books.json", "r") as json_file:
+        json_books_feateres = json_file.read()
+    json_books_feateres = json.loads(json_books_feateres)
+    for number, books_feateres in enumerate(json_books_feateres):
+        env = Environment(
+            loader=FileSystemLoader('.'),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+        template = env.get_template('template.html')
+        rendered_page = template.render(
+            books_feateres=list(chunked(books_feateres, 2)),
+        )
+        current_dir = Path.cwd() / 'pages'
+        Path(current_dir).mkdir(parents=True, exist_ok=True)
+        file_name = f'index{number}.html'
+        filepath = Path() / current_dir / file_name 
+        with open(filepath, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
